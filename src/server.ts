35a736300b9dba1da84e15c9d82ddb12ef3fd936
Luisa Barbalho <luisa.barbalhops@gmail.com>
2020-10-13 19:23:46 -0300

    add general  error message

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, { response } from 'express';
 import path from 'path';
+import 'express-async-errors';
 
 import './database/connection';
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 app.use(express.json());
@@ -10,5 +12,8 @@ app.use(routes);
 
 // Acessar imagens pela url apresentada pela view
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+// Erro
+app.use(errorHandler);
 
 app.listen(3333);
