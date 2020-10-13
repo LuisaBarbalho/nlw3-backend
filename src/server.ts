2cefa56ea0d5e677bd647b73d1ed2c822746ef44
Luisa Barbalho <luisa.barbalhops@gmail.com>
2020-10-13 10:33:44 -0300

    database connection

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,12 @@
 import express, { response } from 'express';
 
-// Cria uma aplicação
 const app = express();
-
-// Para que o express leia o json
 app.use(express.json());
 
-// Rota = conjunto
-// Recurso = usuario
-// Método HTTP = GET, POST, PUT, DELETE
+import './database/connection';
 
-// Parâmetros
-// Query: http://localhost:port/rota?parametro=valor -> ex.: http://localhost:3333/users?search=luisa
-// Route Params (Identificar um recurso): http://localhost:port/rota/parametro -> ex.: http://localhost:3333/users/1 -> app.get('/users/:id', () => ... 
-// Body (formulário, informações mais compostas) 
-
-// O segundo parâmetro recebe automaticamente dois parametros: requests, response
 app.get('/users/:id', (request, response) => {
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
-
     return response.json({message: 'Hello World'});
 });
 
-// Porta 3333 -> localhost:3333
 app.listen(3333);
-
-// Requisição -> Resposta
-// Express - framework simples em Node
