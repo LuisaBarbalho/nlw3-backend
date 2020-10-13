d74c706b09ff03c7513bd77b40d3b66fd16ff3a7
Luisa Barbalho <luisa.barbalhops@gmail.com>
2020-10-13 18:50:48 -0300

    image upload

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -20,6 +20,9 @@ export default {
     },
 
     async create(request: Request, response: Response){
+        console.log(request.files);
+
+
         const {
             name,
             latitude,
@@ -31,6 +34,13 @@ export default {
         } = request.body;
     
         const orphanagesRepository = getRepository(Orphanage);
+
+        // Instruindo que é um array de arquivos - upload de multiplos arquivos
+        const requestImages = request.files as Express.Multer.File[];
+        
+        const images = requestImages.map(image => {
+            return { path: image.filename}
+        })
     
         const orphanage = orphanagesRepository.create({
             name,
@@ -39,7 +49,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
         });
     
         await orphanagesRepository.save(orphanage);
